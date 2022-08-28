fe38741d4aae12dfb2f2616f49b6cb1aa1e1c5f4
Eyp <eyupnl2022@gmail.com>
2022-08-28 01:06:30 +0100

    React Router structure created

--- project_e-commerce-card/src/App.js
+++ project_e-commerce-card/src/App.js
@@ -1,10 +1,25 @@
 import Navbar from "./components/navbar";
 import "./Styles.css";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductList from "./components/productList";
+import ProductDetail from "./components/productDetail";
+
 export default function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Switch>
+          <Navbar />
+          <Route path="/" exact component={ProductList}></Route>
+          <Route
+            path="/product/:productId"
+            exact
+            component={ProductDetail}
+          ></Route>
+          <Route> 404 Not Found</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
