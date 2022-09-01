862acd4c3376bb361b60b1e83286a33b944ddd79
Eyp <eyupnl2022@gmail.com>
2022-09-01 15:38:44 +0100

    Private Chat App created

---
+++ private-chat-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import firebase from "firebase/app";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
